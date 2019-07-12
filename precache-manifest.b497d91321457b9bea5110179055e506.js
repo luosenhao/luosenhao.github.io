e2fe108924dab7bf02735a9a595de67414d28b22
unknown <luosh@bpg.com>
2019-07-12 09:58:50 +0800

    按需加载打包后

---
+++ precache-manifest.b497d91321457b9bea5110179055e506.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18904ec0475fe62716222b8f3114dc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1b8786a22958dacb0f2",
+    "url": "/static/css/2.765df406.chunk.css"
+  },
+  {
+    "revision": "6cda2de2475b73df3446",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "a1b8786a22958dacb0f2",
+    "url": "/static/js/2.a661fef0.chunk.js"
+  },
+  {
+    "revision": "6cda2de2475b73df3446",
+    "url": "/static/js/main.20843f30.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.b497d91321457b9bea5110179055e506.js"
 );
 
 self.addEventListener('message', (event) => {
